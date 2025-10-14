6be718450c25496a20415c3f5e2f8af951053e7a
Benjamin Claveau <benjamin.claveau.1@ulaval.ca>
2025-10-14 15:32:23 -0400

    devoir 2 fini

--- devoir2/distribution_composantes_FFT.py
+++ devoir2/distribution_composantes_FFT.py
@@ -1,42 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.fft as fft
-from scipy.special import factorial
+from scipy.special import factorial, binom
 
 
 # parametre du graphe
-a = 0.5
+a_list = [0.1, 0.6]
 
 # parametres de la methode
 r = 1
-n = 1000
+n = 30
 
 # generation des n points equidistants sur le cercle de rayon r
 points = r*np.exp(1j*2*np.pi * np.array(range(n), dtype=complex) / n)
 
-# calcul des valeurs de H_1(x)
-h1_valeurs = []
-for x in points:
-    polynome = np.array([a**2, -2*a, 1, -x*(1-a)**2])
-    racines = np.roots(polynome)
-    h1_valeurs.append(racines[0])
-h1_valeurs = np.array(h1_valeurs)
 
-# valeurs de H_0(x) a partir des valeurs de H_1(x)
-h0_valeurs = (1-a)*points / (1 - a * h1_valeurs)
+plt.figure(figsize=(6, 5))
 
-# transformee de fourier inverse
-tailles = np.array(range(n)) + 1
+for a in a_list:
+    # calcul des valeurs de H_1(x)
+    h1_valeurs = []
+    for x in points:
+        polynome = np.array([a**2, -2*a, 1, -x*(1-a)**2])
+        racines = np.roots(polynome)
+        # mettre les racines nulles en dehors du cercle unitaire
+        racines[np.where(racines==0)] = 10
+        print(racines)
+        bon_indice = np.argmin(np.absolute(racines))
+        h1_valeurs.append(racines[bon_indice])
+        print(racines[bon_indice])
+    h1_valeurs = np.array(h1_valeurs)
 
-dist_comp = fft.ifft(h0_valeurs)
+    # valeurs de H_0(x) a partir des valeurs de H_1(x)
+    h0_valeurs = (1-a)*points / (1 - a * h1_valeurs)
 
-tailles = np.array(range(len(dist_comp))) + 1
+    # transformee de fourier inverse
+    tailles = np.array(range(n)) + 1
+    dist_comp = 1/(n*r**tailles[:-1]) * fft.fft(h0_valeurs)[1:]
 
-# distribution exacte
-dist_comp_exacte = np.concatenate([[1-a], factorial(3*tailles[1:] - 3) / (factorial(2*tailles[1:]-1)*factorial(tailles[1:] - 1)) * (1-a)**(2*tailles[1:]-1) * a**(tailles[1:]-1)])
+    # distribution exacte
+    dist_comp_exacte = np.concatenate([[1-a], 1/(3*tailles[1:] - 2)*binom(3*tailles[1:] - 2, 2*tailles[1:] - 1) * (1-a)**(2*tailles[1:]-1) * a**(tailles[1:]-1)])
 
-nmax_visible = 100
-plt.plot(tailles[0:nmax_visible], np.abs(dist_comp[0:nmax_visible]/dist_comp[0]), label='méthode FFT')
-plt.plot(tailles[0:nmax_visible], dist_comp_exacte[0:nmax_visible]/dist_comp_exacte[0], label='distribution exacte')
+    print(np.sum(dist_comp_exacte))
+
+    plt.plot(tailles[:10], dist_comp[:10], '+', label=f'FFT a={a}')
+    plt.plot(tailles[:10], dist_comp_exacte[:10], 'x', label=f'Exacte a={a}')
+
 plt.legend()
+plt.ylabel('$\\pi_s$')
+plt.xlabel('$s$')
+plt.savefig('comparaison_fft.pdf')
 plt.show()
