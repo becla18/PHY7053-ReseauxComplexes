34a8deb73db950d734bea995082ffa8c63d10d59
Benjamin Claveau <benjamin.claveau.1@ulaval.ca>
2025-10-25 15:51:49 -0400

    figure 2 du 2e du devoir 3

--- devoir3/2e_fig2.py
+++ devoir3/2e_fig2.py
@@ -9,7 +9,12 @@ t0, t1 = (0, 20)
 time = np.linspace(t0, t1, 1000)
 i0 = [0.01]
 
+# compute analytical trajectories
+def i_analytical(t, i0, R0):
+    return (i0*(R0-1)*np.exp((R0 - 1)*t)) / (R0*(1-i0) - 1 + i0*R0*np.exp((R0 - 1)*t))
+
 i_t_list = []
+i_t_analytical_list = []
 
 for R0 in R0_list:
     # dynamical equation
@@ -20,13 +25,13 @@ for R0 in R0_list:
     sol = solve_ivp(i_dot, (t0, t1), i0, args=(R0,), t_eval = time)
 
     i_t_list.append(sol.y[0])
+    i_t_analytical_list.append(i_analytical(time, i0[0], R0))
 
 plt.figure()
 for i, R0 in enumerate(R0_list[::-1]):
     plt.plot(time, i_t_list[-1-i], label=f"$R_0 = {R0}$")
-    if R0 > 1:
-        plt.plot(time, time*0+(1 - 1/R0), '--', color='#353535ff', linewidth=1.2)
+    plt.plot(time, i_t_analytical_list[-1-i], '--', color='#353535ff', linewidth=1.2)
 plt.xlim(t0, t1)
 plt.legend()
-plt.savefig('2e_a.pdf')
+plt.savefig('2e_a_fig2.pdf')
 plt.show()
